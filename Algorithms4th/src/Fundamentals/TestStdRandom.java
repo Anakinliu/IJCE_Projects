cac9d9d74ee131220494faea2d55421d8f45c82d
ture_true <gugeliuyinquan@gmail.com>
2017-10-18 17:08:38 +0800

    start Algorithms 4

--- Algorithms4th/src/Fundamentals/TestStdRandom.java
+++ Algorithms4th/src/Fundamentals/TestStdRandom.java
@@ -1,5 +1,8 @@
 package Fundamentals;
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 /**
  * Created by Anakinliu on 17.10.18.
  * If you only do what you can do,
@@ -7,6 +10,7 @@ package Fundamentals;
  */
 public class TestStdRandom {
     public static void main(String[] args) {
-        
+        for (int i = 0; i < 10; i++)
+            StdOut.printf("%2$.15s\n", StdRandom.bernoulli(0.5), "i'm the second argument");
     }
 }
