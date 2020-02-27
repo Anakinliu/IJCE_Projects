7cf312c36a4ce76ff9de003cf2abee01fa309ec7
anakinliu <gugeliuyinquan@gmail.com>
2020-02-27 18:03:04 +0800

    Codewars 2020/2/27

--- CodeWars/src/kyu6/Financing_Plan_on_Planet_XY140Zn.java
+++ CodeWars/src/kyu6/Financing_Plan_on_Planet_XY140Zn.java
@@ -7,26 +7,47 @@ import java.math.BigInteger;
     TIME: 2020/2/26
     GOOD LUCK AND NO BUG.
 */
+/*
+0 3 9 18 30
+0 1+1*2 2+2*2+3 3+3*2
+ */
 public class Financing_Plan_on_Planet_XY140Zn {
 
     public static BigInteger finance(int n) {
         // your code
-        if (n == 1) {
+        if (n == 0) {
             return BigInteger.ZERO;
         }
-        StringBuilder sb = new StringBuilder();
-        while (n > 1) {
-            for (int i = n - 1; i <= 2 * (n - 1); i++) {
-                sb.append(i);
+
+        BigInteger sum = new BigInteger("0");
+
+//        for (int i = n ; i <= 2 * n; i++) {
+//            sum = sum.add(new BigInteger(Integer.toString(i)));
+//        }
+
+        for (int i = 1; i <= n; i++) {
+            int single = i * 3;
+            int test = 0;
+            if (i % 2 == 0) {
+                // valueOf方法接收long，自然可以接收int
+                sum = sum.add(
+                        BigInteger.valueOf(single * (i / 2) + i + (i / 2))
+                );
+            } else {
+                System.out.println(test);
+                sum = sum.add(
+                        BigInteger.valueOf(single * (int) Math.ceil(i / 2.0))
+                );
             }
-
-            n--;
         }
-
-        return new BigInteger(sb.toString());
+//        return sum.add(finance(n-1));
+        return sum;
     }
 
+
+
     public static void main(String[] args) {
-        System.out.println(finance(7));
+        System.out.println(finance(6));
+//        finance(6);
     }
 }
